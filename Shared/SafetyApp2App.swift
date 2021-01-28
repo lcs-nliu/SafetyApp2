b2690ea6db4ca598a92b1ec36db95dcec20c2785
Nicole Liu <nicoleliu_@outlook.com>
2021-01-28 11:56:47 -0500

    Initial Commit

---
+++ Shared/SafetyApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  SafetyApp2App.swift
+//  Shared
+//
+//  Created by Liu, Bojun on 2021-01-28.
+//
+
+import SwiftUI
+
+@main
+struct SafetyApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
