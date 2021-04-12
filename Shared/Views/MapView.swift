df99086b6274e111974cd5a6daf3536e2894d300
Nicole Liu <nicoleliu_@outlook.com>
2021-04-11 23:38:32 -0400

    added tips JSON file

--- Shared/Views/MapView.swift
+++ Shared/Views/MapView.swift
@@ -7,49 +7,49 @@
 import MapKit
 import SwiftUI
 
+
 struct MapView: View {
-    
+
     
     // Reference to police station store
     // Derived value
     
     @ObservedObject var store: LocationStore
     
+    
     // Map centered on GTA
     @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 43.6532, longitude: -79.3832), span: MKCoordinateSpan(latitudeDelta: 0.4, longitudeDelta: 0.3))
     
-    
-    
-    
     var body: some View {
-        
-        
-        
         
         Map(coordinateRegion: $region,  showsUserLocation: true, userTrackingMode: .constant(.follow), annotationItems: store.locations) { place in
             
             
+            
+            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude)) {
                 
-                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude)) {
-                    
-                    NavigationLink(destination: LocationDetail(safetyLocation: place)) {
-                        Image(place.type)
-                            .resizable()
-                            .cornerRadius(10)
-                            .frame(width: 20, height: 20)
-                            .shadow(radius:3)
-                    }
-            
-            
+                NavigationLink(destination: LocationDetail(safetyLocation: place)) {
+                    Image(place.type)
+                        .resizable()
+                        .cornerRadius(10)
+                        .frame(width: 20, height: 20)
+                        .shadow(radius:3)
+                }
+                
+            }
             
             
         }
+        .navigationTitle("Toronto Map")
+        
+        
+        
         
     }
-        .navigationTitle("Toronto Map")
     
 }
-}
+
+
 
 
 struct MapView_Previews: PreviewProvider {
